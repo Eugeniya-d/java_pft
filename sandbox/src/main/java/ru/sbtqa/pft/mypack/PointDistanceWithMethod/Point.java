9e4d9371fd4c80bc5c38a5a4b5970420ad58329a
Eugeniya-d <edavydova1992@gmail.com>
2020-10-24 00:06:24 +0300

    Задание с функцией и задание с методом по отдельным пакетам

--- sandbox/src/main/java/ru/sbtqa/pft/mypack/PointDistanceWithMethod/Point.java
+++ sandbox/src/main/java/ru/sbtqa/pft/mypack/PointDistanceWithMethod/Point.java
@@ -1,12 +1,12 @@
-package ru.sbtqa.pft.mypack;
+package ru.sbtqa.pft.mypack.PointDistanceWithMethod;
 
-public class PointWithMethod {
+public class Point {
     public double x1;
     public double y1;
     public double x2;
     public double y2;
 
-    public PointWithMethod(double x1, double y1,double x2,double y2) {
+    public Point(double x1, double y1, double x2, double y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
--- sandbox/src/main/java/ru/sbtqa/pft/mypack/PointDistanceWithMethod/PointDistance.java
+++ sandbox/src/main/java/ru/sbtqa/pft/mypack/PointDistanceWithMethod/PointDistance.java
@@ -1,8 +1,8 @@
-package ru.sbtqa.pft.mypack;
+package ru.sbtqa.pft.mypack.PointDistanceWithMethod;
 
-public class PointDistanceWithMethod {
+public class PointDistance {
     public static void main(String[] args) {
-        PointWithMethod p = new PointWithMethod(1, 3, 2, 6);
+        Point p = new Point(1, 3, 2, 6);
         System.out.println("Расстояние между двумя точками " + "p1" + "(" + p.x1 + "," + p.y1 + ")" + " и p2" + "(" + p.x2 + "," + p.y2 + ") " + "равно " + p.pointDistance());
 
     }
