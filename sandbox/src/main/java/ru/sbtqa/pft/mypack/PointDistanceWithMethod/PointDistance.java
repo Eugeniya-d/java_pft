1e03da58c941ce5068bf991af81f8e40c09a5017
Eugeniya-d <edavydova1992@gmail.com>
2020-10-24 11:44:54 +0300

    Изменения Point для метода

--- sandbox/src/main/java/ru/sbtqa/pft/mypack/PointDistanceWithMethod/PointDistance.java
+++ sandbox/src/main/java/ru/sbtqa/pft/mypack/PointDistanceWithMethod/PointDistance.java
@@ -2,8 +2,9 @@ package ru.sbtqa.pft.mypack.PointDistanceWithMethod;
 
 public class PointDistance {
     public static void main(String[] args) {
-        Point p = new Point(1, 3, 2, 6);
-        System.out.println("Расстояние между двумя точками " + "p1" + "(" + p.x1 + "," + p.y1 + ")" + " и p2" + "(" + p.x2 + "," + p.y2 + ") " + "равно " + p.pointDistance());
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(2, 4);
+        System.out.println("Расстояние между двумя точками " + "p1" + "(" + p1.x + "," + p1.y + ")" + " и p2" + "(" + p2.x + "," + p2.y + ") " + "равно " + p1.pointDistance(p2));
 
     }
 }
