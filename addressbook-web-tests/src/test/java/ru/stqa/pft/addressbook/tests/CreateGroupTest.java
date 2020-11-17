3c4f58ef0c6493a1d233eaf96ba51c6608962600
Eugeniya-d <edavydova1992@gmail.com>
2020-11-17 15:07:09 +0300

    удаление/модификация последнего элемента списка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTest.java
@@ -4,16 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class CreateGroupTest extends TestBase {
 
 
     @Test
     public void testCreateGroup() {
         app.getNavigationHelper().goToGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().createGroup(new GroupData("18MyTestGroup", "Test", "Test"));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().createGroup(new GroupData("MyTestGroup", "test", "test"));
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
 
 }
