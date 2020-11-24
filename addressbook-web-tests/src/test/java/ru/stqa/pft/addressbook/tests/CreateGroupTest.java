6fbd5d92d5229504923f2ad108ab919197a972c6
Eugeniya-d <edavydova1992@gmail.com>
2020-11-24 16:13:19 +0300

    5.6 Fluent проверки для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateGroupTest.java
@@ -2,8 +2,7 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-
-import java.util.Set;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,16 +13,15 @@ public class CreateGroupTest extends TestBase {
     @Test
     public void testCreateGroup() {
         app.goTo().groupPage();
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData group = new GroupData().withName("MyTestGroup").withHeader("Test").withFooter("Test");
         app.group().create(group);
-        Set<GroupData> after = app.group().all();
+        Groups after = app.group().all();
         assertThat(after.size(),equalTo( before.size() + 1));
 
 
-        group.withId(after.stream().mapToInt(g -> g.getId()).max().getAsInt());
-        before.add(group);
-        assertThat(after, equalTo(before.withAdded(group)));
+        assertThat(after, equalTo(
+                before.withAdded(group.withId(after.stream().mapToInt(g -> g.getId()).max().getAsInt()))));
     }
 
 }
