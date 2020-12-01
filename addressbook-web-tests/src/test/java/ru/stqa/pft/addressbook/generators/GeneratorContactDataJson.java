442fbcc8a13842eebf93cc78598614c3a8a73afd
Eugeniya-d <edavydova1992@gmail.com>
2020-12-01 21:03:32 +0300

    6.2 Contacts & Groups

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GeneratorContactDataJson.java
@@ -0,0 +1,40 @@
+package ru.stqa.pft.addressbook.generators;
+
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorContactDataJson {
+
+    public static void main(String[] args) throws IOException {
+        int count = Integer.parseInt(args[0]);
+        File file = new File(args[1]);
+
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, file);
+    }
+
+    private static void save(List<ContactData> contacts, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s,%s,%s,%s\n", contact.getName(), contact.getSurname(), contact.getMobilePhone(),contact.getEmail()));
+        }
+        writer.close();
+    }
+
+    private static List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withName(String.format("Ivan"))
+                    .withSurname(String.format("Ivanov%s", i))
+                    .withMobilePhone(String.format("+7999999999%s", i)).withEmail(String.format("%s7999999999@mail.ru", i)));
+        }
+        return contacts;
+    }
+}
+
