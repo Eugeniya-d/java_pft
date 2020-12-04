90c6fc420d2a573698469849cae58923cff120d8
Eugeniya-d <edavydova1992@gmail.com>
2020-12-04 16:42:26 +0300

    Задание №14

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -6,10 +6,19 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-    protected static final ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser"));
+    protected static ApplicationManager app;
+
+    static {
+        try {
+            app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
