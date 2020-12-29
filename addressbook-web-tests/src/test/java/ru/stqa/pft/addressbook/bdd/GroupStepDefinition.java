3b29d509b908adcac55a3ad85abf953ca0e5abcb
Eugeniya-d <edavydova1992@gmail.com>
2020-12-29 16:54:48 +0300

    Исправлена зависимость testNG

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/bdd/GroupStepDefinition.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/bdd/GroupStepDefinition.java
@@ -5,8 +5,7 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
+
 import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -14,39 +13,48 @@ import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.IOException;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class GroupStepDefinition {
+
     private ApplicationManager app;
     private Groups groups;
     private GroupData newGroup;
+
     @Before
+    // Аннотация из пакета cucumber.api.java
     public void init() throws IOException {
         app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
         app.init();
     }
 
     @After
+    // Аннотация из пакета cucumber.api.java
     public void stop() {
         app.stop();
+        // Окончательное уничтожение объекта
         app = null;
     }
 
     @Given("^a set of groups$")
+    // Поиск по точному соответсвию: ^ - начало фрагмента, $ - конец фрагмента
     public void loadGroups() {
         groups = app.db().groups();
     }
 
-    @When("^I create a new group with name (.+), with header (.+) and footer (.+)$")
+    @When("^I create a new group with name (.+), header (.*) and footer (.+)$")
+    // (.+) - эта группа обозначает параметр
     public void createGroup(String name, String header, String footer) {
         newGroup = new GroupData().withName(name).withHeader(header).withFooter(footer);
         app.goTo().groupPage();
         app.group().create(newGroup);
     }
 
-    @Then("^the new set of groups is equal to the old set with added group$")
+    @Then("^the new set of groups is equal to the old set with the added group$")
     public void verifyGroupCreated() {
         Groups newGroups = app.db().groups();
-        MatcherAssert.assertThat(newGroups, CoreMatchers.equalTo(groups
-                .withAdded(newGroup.withId(newGroups.stream().mapToInt(g -> g.getId()).max().getAsInt()))));
-
+        assertThat(newGroups, equalTo(
+                groups.withAdded(newGroup.withId(newGroups.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 }
